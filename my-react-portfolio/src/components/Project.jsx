b488e7e23bf354347cb01b692824fd62beebba47
unknown <sandra.gutierrez.ru@gmail.com>
2023-06-17 19:22:44 -0500

    added projects and styling to portfolio

--- my-react-portfolio/src/components/Project.jsx
+++ my-react-portfolio/src/components/Project.jsx
@@ -1,4 +1,6 @@
-import React from "react"
+import React from 'react'
+import Foodie from '../assets/images/foodie-fetch-screenshot.png'
+import Jate from '../assets/images/JATEpwa.gif'
 
 const Projects = () => {
     return (
@@ -10,28 +12,52 @@ const Projects = () => {
                     <p className='py-6'>Check out some of my recent projects here!</p>
                 </div>
 
+                {/* Project cards container */}
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                    {/* Card with Project */}
-                    <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                        {/* hover effect */}
-                        <div>
-                            <div>
-                                <span>
 
+                    {/* Card with Project */}
+                    <div style={{backgroundImage: `url(${Foodie})`}}
+                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                        
+                        {/* hover effect */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <div>
+                                <span className='text-2xl font-bold text-white tracking-wider'>
+                                    Third-Party API
                                 </span>
-                                <div>
+                                <div className='pt-8 text-center'>
                                     <a href="/">
-                                        <button></button>
+                                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                                     </a>
                                     <a href="/">
-                                        <button></button>
+                                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                                     </a>
                                 </div>
                             </div>
                         </div>
-
                     </div>
-                    
+                    {/* Card with Project */}
+                    <div style={{backgroundImage: `url(${Jate})`}}
+                    className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                        
+                        {/* hover effect */}
+                        <div className='opacity-0 group-hover:opacity-100'>
+                            <div>
+                                <span className='text-2xl font-bold text-white tracking-wider'>
+                                    Progressive Web App
+                                </span>
+                                <div className='pt-8 text-center'>
+                                    <a href="/">
+                                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
+                                    </a>
+                                    <a href="/">
+                                        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             
             </div>
